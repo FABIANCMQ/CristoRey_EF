075458aee54f598c62877d940c895d301304a1b9
FabianCMQ <fab.castmurga@gmail.com>
2026-06-16 01:38:46 -0500

    UsuarioControlador completado.

--- src/cristorey_ef/UsuarioControlador.java
+++ src/cristorey_ef/UsuarioControlador.java
@@ -30,6 +30,36 @@ public class UsuarioControlador {
                 doc1
             )
         );
+        
+        //Creamos un planillero 
+        Documento doc2 = new Documento("DNI","87654321",LocalDate.of(2025, 2, 12),
+        LocalDate.of(2033, 2, 12),"Nacional",28);
+
+        usuario.add(
+            new Planillero(
+                "Operaciones",
+                "Luis Torres",
+                "planillero@cristorey.com",
+                "plan123",
+                doc2
+            )
+        );
+        
     }
-    
+
+    public ArrayList<Usuario> getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(ArrayList<Usuario> usuario) {
+        this.usuario = usuario;
+    }
+    public Usuario buscarNombre(String nombre){
+        for (int i = 0; i < usuario.size(); i++) {
+            if (usuario.get(i).getNombre().equalsIgnoreCase(nombre)) {
+                return usuario.get(i);
+            }
+        }
+        return null;
+    }
 }
